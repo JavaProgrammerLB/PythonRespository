9e1d40a1c889b97c24e9806d6d5efd23e0767635
LiuBei <liubeitongxue@icloud.com>
2015-11-18 18:03:55 +0800

    commit a python file

--- SRC/1235813.py
+++ SRC/1235813.py
@@ -1,7 +1,7 @@
 def main():
     list=[1,1]
     for i in range(40):
-         list.append(list[i]+list[i+1])
+         list.append(list[i] + list[i+1])
          if list[i]>4000000:
              #if list[i-1]<4000000:
                  print(i-1, list[i-1])
---
+++ SRC/make_text_file.py
@@ -0,0 +1,28 @@
+'make_text_file.py  -----  create text file by xiaoyuan'
+
+import os
+ls = os.linesep
+
+while True:
+    if os.path.exists(fname):
+        print "ERROR:'%s' already exists" % fname
+    else:
+        break
+
+#get file content lines
+all = []
+print "\nEnter lines ('.' by itself to quit).\n"
+
+#loop until user terminates input
+while Ture:
+    entry = raw_input('> ')
+    if entry == '.':
+        break
+    else:
+        all.append(entry)
+
+#write lines to file with proper line-ending
+fobj = open(fname,'w')
+fobj.writelines(['%s%s' % (x,ls) for x in all])
+fobj.close()
+print 'DONE!'
